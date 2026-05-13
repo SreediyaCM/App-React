1a4f4bc5967e05e6010be2106a0df36750b3cbb0
Demo <sreediyacm@gmail.com>
2026-05-13 14:46:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import {BrowserRouter, Link, Route, Routes} from 'react-router-dom'
+import './App.css'
+import Home from './pages/Home'
+import Features from './pages/Features'
+import Community from './pages/Community'
+import Blog from './pages/Blog'
+import Pricing from './pages/Pricing'
+
+function App() {
+ 
+  return (
+    <>
+      <BrowserRouter> 
+      <header>
+      <nav>
+        <Link to="/">Home</Link><br /> 
+        <Link to="/Features">Features</Link><br />
+        <Link to="/Community">Community</Link><br />
+        <Link to="/Blog">Blog</Link><br />
+        <Link to="Pricing">Pricing</Link><br />
+      </nav>
+      </header>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/Features' element={<Features/>}/>
+        <Route path='/Community' element={<Community/>}/>
+        <Route path='/Blog' element={<Blog/>}/>
+        <Route path='/Pricing' element={<Pricing/>}/>
+      </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Features.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+function Features() {
+ 
+  return (
+    <>
+    <div>
+      <h1>Welcome</h1>
+    </div>
+    </>
+  )
+}
+
+export default Features
